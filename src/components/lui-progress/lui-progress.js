ee3bfe6a3c4850b36397fa364611f3c9258f2b47
cenfun <cenfun@qq.com>
2022-05-24 17:53:21 +0800

    update progress

--- src/components/lui-progress/lui-progress.js
+++ src/components/lui-progress/lui-progress.js
@@ -1,6 +1,6 @@
 import { html } from 'lit';
 
-//import Util from '../../util/util.js';
+import Util from '../../util/util.js';
 
 import LuiBase from '../../base/lui-base.js';
 import componentStyle from './lui-progress.scss';
@@ -11,8 +11,34 @@ export default class extends LuiBase {
     static properties = {
         percentage: {
             type: Number
+        },
+        width: {
+            type: String
+        },
+        height: {
+            type: String
+        },
+        radius: {
+            type: String
+        },
+        color: {
+            type: String
+        },
+        borderColor: {
+            attribute: 'border-color',
+            type: String
         }
     };
+
+    constructor() {
+        super();
+        this.percentage = 0;
+        this.width = '100px';
+        this.height = '15px';
+        this.radius = '3px';
+        this.color = '#ccc';
+        this.borderColor = '#aaa';
+    }
 
     connectedCallback() {
         super.connectedCallback();
@@ -20,7 +46,14 @@ export default class extends LuiBase {
     }
     
     render() {
-        return html`<div class="lui-progress" style="background-size:${this.percentage}%;"></div>`;
+        const stm = {
+            'width': this.width,
+            'height': this.height,
+            'border-radius': this.radius,
+            'border-color': this.borderColor,
+            'background': `linear-gradient(${this.color} 0 0) 0/${this.percentage}% no-repeat`
+        };
+        return html`<div class="lui-progress" style="${Util.styleMap(stm)}"></div>`;
     }
 
 }
